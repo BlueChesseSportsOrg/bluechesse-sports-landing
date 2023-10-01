23795128f2036aef7e934a26f73a5cdfc78932fc
Worlanyo <worladamalie@gmail.com>
2023-10-01 10:40:32 +0000

    fix for fixing broken images on github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+let assetPrefix = ''
+let basePath = ''
+
+let isGithubActions = process.env.BLUECHEESE_ACTIONS || false
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+  
+    assetPrefix = `/bluechesse-sports-landing/`
+    basePath = `/bluechesse-sports-landing` 
+    
+  }
+
+
 const nextConfig = {
-    output: "export",
+   output: "export",
+   assetPrefix: assetPrefix,
+   basePath: basePath,
 }
 
 module.exports = nextConfig
