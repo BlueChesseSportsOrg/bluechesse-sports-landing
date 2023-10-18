ccec89f5d3a72675d911b100c9e5e4c4ac4e6b18
Worlanyo <worladamalie@gmail.com>
2023-10-18 12:27:19 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 let assetPrefix = process.env.BASE_PATH
 let basePath = process.env.BASE_PATH
 
-let isGithubActions = process.env.BLUECHEESE_ACTIONS || false
+// let isGithubActions = process.env.BLUECHEESE_ACTIONS || false
 
-if (isGithubActions) {
-    // trim off `<owner>/`
+// if (isGithubActions) {
+//     // trim off `<owner>/`
   
-    assetPrefix = `/bluechesse-sports-landing/`
-    basePath = `/bluechesse-sports-landing` 
+//     assetPrefix = `/bluechesse-sports-landing/`
+//     basePath = `/bluechesse-sports-landing` 
     
-  }
+//   }
 
 
 const nextConfig = {
